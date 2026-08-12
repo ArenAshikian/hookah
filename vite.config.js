1ec3b248a021dfb9b4735bb0b284ac503700f916
Aren Ashikian <akashiki@usc.edu>
2026-08-11 17:13:38 -0700

    Configure Vite for informational React site

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// Build config for an informational event-services website. No ecommerce or payment functionality.
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
